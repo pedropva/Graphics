a969c9d2aa3c4bd0c528fdedf13873651e15c321
pedropva <idlol2@hotmail.com>
2018-03-26 01:04:33 -0300

    Its reading from a file now, can draw points,lines and polygons.

--- canvas.js
+++ canvas.js
@@ -1,19 +1,19 @@
 
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
-var file ="file:///C:/your/path/to/file.txt";
+var file ="./draw.txt";
 var grd; //holds gradient data
 /*FONTS:
 https://www.w3schools.com/tags/ref_canvas.asp
 https://stackoverflow.com/questions/14446447/how-to-read-a-local-text-file
 https://www.w3schools.com/html/html5_canvas.asp
 */
+readTextFile(file);
 function drawPoint(x,y){
 	ctx.moveTo(x, y);
-	ctx.lineTo(x, y);
+	ctx.fillRect(x,y,5,5);
 	ctx.stroke();
 }
-
 function drawLine(Sx,Sy,Ex,Ey){//starting x, starting y, ending x, ending y
 	ctx.moveTo(Sx, Sy);
 	ctx.lineTo(Ex, Ey);
@@ -29,9 +29,9 @@ function drawBezier(Sx,Sy,C1x,C1y,C2x,C2y,Ex,Ey){//C1 and C2: Control point 1 an
 	ctx.stroke();
 }
 
-function drawArc(Cx,Cy,Sa,Ea){//Sa and Ea: starting and Ending angle, C: center
+function drawArc(Cx,Cy,R,Sa,Ea){//Sa and Ea: starting and Ending angle, C: center R:Radius
 	ctx.beginPath();
-	ctx.arc(Cx,Cy,Sa,Ea);//Example: (100,75,50,0*Math.PI,1.5*Math.PI
+	ctx.arc(Cx,Cy,R,Sa,Ea);//Example: (100,75,50,0*Math.PI,1.5*Math.PI
 	ctx.stroke();
 }
 function drawText(text,x,y){
@@ -72,5 +72,64 @@ function readTextFile(file)
     rawFile.send(null);
 }
 function drawFromFile(data){
-	alert("foi:"+data);
+	var curLine="";
+	var nLine=0;
+	data = data.toLowerCase();
+	var dataSize = parseInt(data.length);
+	curMode = "none";
+	while(nLine< dataSize ){//while we're not in the end of file 
+		curLine = "";
+		while(data[nLine]!='\n' && nLine < dataSize ){//read each line
+			curLine += data[nLine];
+			nLine++;
+		}
+		nLine++;//go to next line
+		//now we have the entire line ready to process
+		if(curLine.indexOf("point") !== -1){
+			curMode = "point";
+		}else if(curLine.indexOf("line") !== -1){
+			curMode = "line";
+		}else if(curLine.indexOf("polygon") !== -1){
+			curMode = "polygon";
+		}else if(curLine.indexOf("arc") !== -1){
+			curMode = "arc";
+		}else if(curLine.indexOf("bezier") !== -1){
+			curMode = "bezier";
+		}else if(curLine.indexOf("text") !== -1){
+			curMode = "text";
+		}else{//if its not a mode-changing  line then its a element line
+			switch(curMode){
+				case "point":
+					curLine = curLine.split(",");
+					drawPoint(curLine[0],curLine[1]);
+					break;
+				case "line":
+					curLine = curLine.split(",");
+					drawLine(curLine[0],curLine[1],curLine[2],curLine[3]);
+					break;
+				case "polygon":
+					curLine = curLine.split(",");
+					for (var i = 0; i <= parseInt(curLine.length)/2; i=i+2) {
+						if(i==0){
+							drawLine(curLine[i],curLine[i+1],curLine[i+2],curLine[i+3]);
+						}else{
+							drawLine(curLine[i],curLine[i+1],curLine[i+2],curLine[i+3]);
+						}
+					}
+					drawLine(curLine[i],curLine[i+1],curLine[0],curLine[1]);
+					break;
+				case "arc":
+					
+					break;
+				case "bezier":
+					
+					break;
+				case "text":
+					
+					break;
+				default:
+					//?
+			}
+		}
+	}	
 }
